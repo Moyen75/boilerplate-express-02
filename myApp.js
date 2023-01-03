464634a241ec0a8f7e7fb30742372654e244d671
moyen75 <moyenislam75@gmail.com>
2023-01-03 13:15:59 +0600

    edit api

--- myApp.js
+++ myApp.js
@@ -1,39 +1,10 @@
 let express = require('express');
 let app = express();
+app.use(express.json())
+
+app.get("/json", (req, res) => {
+    res.json({ "message": "Hello json" })
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
